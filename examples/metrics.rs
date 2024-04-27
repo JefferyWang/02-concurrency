9c279f9bfac6697d5effed23608d48908b62fa6e
JefferyWang <admin@wangjunfeng.com>
2024-04-27 20:16:24 +0800

    feature: support rwlock and display

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -24,7 +24,7 @@ fn main() -> Result<()> {
 
     loop {
         thread::sleep(Duration::from_secs(2));
-        println!("metrics: {:?}", metrics.snapshot());
+        println!("metrics: {}", metrics);
     }
 }
 
--- src/metrics.rs
+++ src/metrics.rs
@@ -4,25 +4,36 @@
 use anyhow::{anyhow, Result};
 use std::{
     collections::HashMap,
-    sync::{Arc, Mutex},
+    fmt::Display,
+    sync::{Arc, RwLock},
 };
 
 #[derive(Debug, Clone, Default)]
 pub struct Metrics {
-    data: Arc<Mutex<HashMap<String, i64>>>,
+    data: Arc<RwLock<HashMap<String, i64>>>,
+}
+
+impl Display for Metrics {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let data = self.data.read().map_err(|_e| std::fmt::Error {})?;
+        for (key, value) in data.iter() {
+            writeln!(f, "{}: {}", key, value)?;
+        }
+        Ok(())
+    }
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Self {
-            data: Arc::new(Mutex::new(HashMap::new())),
+            data: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
         let mut data = self
             .data
-            .lock()
+            .write()
             .map_err(|e| anyhow!("lock failed: {:?}", e))?;
         let counter = data.entry(key.into()).or_insert(0);
         *counter += 1;
@@ -30,14 +41,14 @@ impl Metrics {
     }
 
     pub fn dec(&mut self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
         let counter = data.entry(key.into()).or_insert(0);
         *counter -= 1;
         Ok(())
     }
 
     pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
-        let data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let data = self.data.read().map_err(|e| anyhow!(e.to_string()))?;
         Ok(data.clone())
     }
 }
